43bedbdd586a629df73a78cdf24a6203e573277b
Mark-Dang <markd4@uci.edu>
2025-08-07 18:17:54 -0700

    Added function toggleSystem that changes mode between ON (armed) and OFF (unarmed); function not complete

--- Laser Security System/src/main.cpp
+++ Laser Security System/src/main.cpp
@@ -1,19 +1,41 @@
 #include <Arduino.h>
 
+typedef enum {OFF = 0, ON = 1} systemState; // state of 
+typedef enum {laserDiode = 25, pushButton = 33, photoResistor = 21, LED = 36, buzzer = 37} ioPins;
+
 // put function declarations here:
+void toggleSystem(); // Turns on trip wire security system
+void calibrateLDR(); // Calibrate photoresistor to detect when the laser diode's beam is broken
+
+
+systemState sysState = OFF;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(25, OUTPUT); //GPIO output for laser diode
-  pinMode(33, INPUT); //Push button input to activate alarm
-  digitalWrite(25, HIGH);
+  // set pin modes
+  pinMode(laserDiode, OUTPUT); // GPIO output for laser diode
+  pinMode(pushButton, INPUT); // Push button input to activate alarm
+  pinMode(photoResistor, INPUT), // Photoresistor
+
+  pinMode(LED, OUTPUT); // Pin for test LED
+
+  //set initial state
+  digitalWrite(laserDiode, LOW);
 }
 
 void loop() {
   if (digitalRead(33) == HIGH) {
-    digitalWrite(25, !digitalRead(25));
+    toggleSystem();
     delay(250);
   }
 }
 
 // put function definitions here:
+void toggleSystem() {
+  switch(sysState) {
+    case(OFF):
+      sysState = ON;
+    case(ON):
+      sysState = OFF;
+  }
+  digitalWrite(laserDiode, !digitalRead(laserDiode));
+}
