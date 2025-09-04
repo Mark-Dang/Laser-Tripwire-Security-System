ac32f909a2c0b399097205ad834227d52840dec8
Mark-Dang <markd4@uci.edu>
2025-09-04 16:41:18 -0700

    System sends information about break-in times to IoT hub hosted using Microsoft Azure

--- Laser Security System/src/main.cpp
+++ Laser Security System/src/main.cpp
@@ -11,13 +11,41 @@
 #include <string>
 
 // for connecting to WiFi and IoT Hub (NOTE: REMOVE BEFORE PUSHING)
-#define WIFI_SSID "" // NOTE: Please delete this value before submitting assignment
+#define WIFI_SSID "UCInet Mobile Access" // NOTE: Please delete this value before submitting assignment
 #define WIFI_PASSWORD "" // NOTE: Please delete this value before submitting assignment
 
 // Azure IoT Hub configuration
-#define SAS_TOKEN ""
+#define SAS_TOKEN "SharedAccessSignature sr=LaserSecuritySystem.azure-devices.net%2Fdevices%2FLaserSecuritySystem&sig=%2F9N3KP0HLWwdGzrtwuhmkKdlXyymGe%2BxKBfmjlqjOyU%3D&se=1757581844"
 // Root CA certificate for Azure IoT Hub
-const char* root_ca = "";
+const char* root_ca = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIEtjCCA56gAwIBAgIQCv1eRG9c89YADp5Gwibf9jANBgkqhkiG9w0BAQsFADBh\n" \
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n" \
+"MjAeFw0yMjA0MjgwMDAwMDBaFw0zMjA0MjcyMzU5NTlaMEcxCzAJBgNVBAYTAlVT\n" \
+"MR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xGDAWBgNVBAMTD01TRlQg\n" \
+"UlMyNTYgQ0EtMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMiJV34o\n" \
+"eVNHI0mZGh1Rj9mdde3zSY7IhQNqAmRaTzOeRye8QsfhYFXSiMW25JddlcqaqGJ9\n" \
+"GEMcJPWBIBIEdNVYl1bB5KQOl+3m68p59Pu7npC74lJRY8F+p8PLKZAJjSkDD9Ex\n" \
+"mjHBlPcRrasgflPom3D0XB++nB1y+WLn+cB7DWLoj6qZSUDyWwnEDkkjfKee6ybx\n" \
+"SAXq7oORPe9o2BKfgi7dTKlOd7eKhotw96yIgMx7yigE3Q3ARS8m+BOFZ/mx150g\n" \
+"dKFfMcDNvSkCpxjVWnk//icrrmmEsn2xJbEuDCvtoSNvGIuCXxqhTM352HGfO2JK\n" \
+"AF/Kjf5OrPn2QpECAwEAAaOCAYIwggF+MBIGA1UdEwEB/wQIMAYBAf8CAQAwHQYD\n" \
+"VR0OBBYEFAyBfpQ5X8d3on8XFnk46DWWjn+UMB8GA1UdIwQYMBaAFE4iVCAYlebj\n" \
+"buYP+vq5Eu0GF485MA4GA1UdDwEB/wQEAwIBhjAdBgNVHSUEFjAUBggrBgEFBQcD\n" \
+"AQYIKwYBBQUHAwIwdgYIKwYBBQUHAQEEajBoMCQGCCsGAQUFBzABhhhodHRwOi8v\n" \
+"b2NzcC5kaWdpY2VydC5jb20wQAYIKwYBBQUHMAKGNGh0dHA6Ly9jYWNlcnRzLmRp\n" \
+"Z2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RHMi5jcnQwQgYDVR0fBDswOTA3\n" \
+"oDWgM4YxaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0R2xvYmFsUm9v\n" \
+"dEcyLmNybDA9BgNVHSAENjA0MAsGCWCGSAGG/WwCATAHBgVngQwBATAIBgZngQwB\n" \
+"AgEwCAYGZ4EMAQICMAgGBmeBDAECAzANBgkqhkiG9w0BAQsFAAOCAQEAdYWmf+AB\n" \
+"klEQShTbhGPQmH1c9BfnEgUFMJsNpzo9dvRj1Uek+L9WfI3kBQn97oUtf25BQsfc\n" \
+"kIIvTlE3WhA2Cg2yWLTVjH0Ny03dGsqoFYIypnuAwhOWUPHAu++vaUMcPUTUpQCb\n" \
+"eC1h4YW4CCSTYN37D2Q555wxnni0elPj9O0pymWS8gZnsfoKjvoYi/qDPZw1/TSR\n" \
+"penOgI6XjmlmPLBrk4LIw7P7PPg4uXUpCzzeybvARG/NIIkFv1eRYIbDF+bIkZbJ\n" \
+"QFdB9BjjlA4ukAg2YkOyCiB8eXTBi2APaceh3+uBLIgLk8ysy52g2U3gP7Q26Jlg\n" \
+"q/xKzj3O9hFh/g==\n" \
+"-----END CERTIFICATE-----\n";
 
 
 String iothubName = "LaserSecuritySystem"; //Your hub name (replace if needed)
@@ -48,6 +76,9 @@ bool getTime(const char *str);
 bool getDate(const char *str);
 void printTime();
 void print2digits(int number);
+void scanNetworks();
+void sendPayload();
+bool isCaptivePortal();
 
 //global variables for laser security system
 systemState sysState;
@@ -68,7 +99,8 @@ void setup() {
   pinMode(LED, OUTPUT); // GPIO output for LED
 
   // connect to WiFi
-  //connectToWiFi();
+  scanNetworks();
+  connectToWiFi();
 
   //configure DS1307 RTC
   configureDS1307();
@@ -82,7 +114,7 @@ void loop() {
   if (digitalRead(33) == HIGH) {
     toggleState();
     runState();
-    delay(250); //delay to not overrun this; can remove
+    delay(250);
   }
 }
 
@@ -174,9 +206,10 @@ bool checkBroken() { // check if path between laser diode and photoresistor is b
         Serial.println("tripwire triggered");
         
         //print the time break in happened; this will be changed to sending to cloud later
-        printTime();
-
         activateAlarm();
+        printTime();
+        sendPayload();
+
         return true;
       }
       return false;
@@ -206,6 +239,12 @@ void connectToWiFi() {
   Serial.println(WiFi.localIP());
   Serial.println("MAC address: ");
   Serial.println(WiFi.macAddress());
+
+  if (isCaptivePortal()) {
+    Serial.println("⚠ Captive portal detected! Need to click 'I Agree'");
+  } else {
+    Serial.println("✅ No captive portal, safe to continue with HTTPS");
+  }
 }
 
 
@@ -303,4 +342,74 @@ void print2digits(int number) {
     Serial.write('0');
   }
   Serial.print(number);
+}
+
+
+void scanNetworks() {
+  Serial.println("🔎 Scanning for WiFi networks...");
+  int n = WiFi.scanNetworks();
+  if (n == 0) {
+    Serial.println("❌ No networks found.");
+  } else {
+    Serial.printf("✅ %d networks found:\n", n);
+    for (int i = 0; i < n; ++i) {
+      Serial.printf("%2d: %s (%d dBm) %s\n", 
+                    i + 1, 
+                    WiFi.SSID(i).c_str(), 
+                    WiFi.RSSI(i), 
+                    (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? "open" : "secured");
+      delay(10);
+    }
+  }
+  Serial.println();
+}
+
+
+void sendPayload() {
+  ArduinoJson::JsonDocument doc;
+  RTC.read(tm);
+  doc["Hour"] = tm.Hour;
+  doc["Minute"] = tm.Minute;
+  doc["Second"] = tm.Second;
+  doc["Month"] = tm.Month;
+  doc["Day"] = tm.Day;
+  doc["Year"] = tm.Year;
+  char buffer[256];
+  serializeJson(doc, buffer, sizeof(buffer));
+
+  // Send telemetry via HTTPS
+  WiFiClientSecure client;
+  client.setCACert(root_ca); // Set root CA certificate
+  HTTPClient http;
+  http.begin(client, url);
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("Authorization", SAS_TOKEN);
+  int httpCode = http.POST(buffer);
+
+  if (httpCode == 204) { // IoT Hub returns 204 No Content for successful telemetry
+      Serial.println("Telemetry sent: " + String(buffer));
+    } else {
+      Serial.println("Failed to send telemetry. HTTP code: " + String(httpCode));
+  }
+  http.end();
+}
+
+
+bool isCaptivePortal() {
+  HTTPClient http;
+  http.begin("http://clients3.google.com/generate_204");
+  int httpCode = http.GET();
+  String payload = http.getString();
+  http.end();
+
+  Serial.printf("HTTP code: %d\n", httpCode);
+
+  // Normal response is 204 with no content
+  if (httpCode == 204) {
+    return false;  // no captive portal
+  }
+  if (httpCode == 200 && payload.length() > 0) {
+    return true;   // captive portal redirected us
+  }
+  return true;     // treat anything else as captive portal
 }
